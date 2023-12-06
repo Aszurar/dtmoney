8593da21de42ef3bb0a9f18640c45d2cc1f548a9
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-06 00:50:27 -0300

    :recycle: Refactor header component with TailwindCSS  :recycle:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,25 +1,19 @@
-import logoIMG from '../../assets/logo.svg'
+import { FiDollarSign } from "react-icons/fi"
 
-import { Container, Content } from './styles'
 
-interface HeaderProps { 
-    onOpenNewTransactionModal: () => void;
-}
-
-export function Header({ onOpenNewTransactionModal } : HeaderProps) {
-
-    return (
-        <>
-            <Container>
-                <Content>
-                    <img src={logoIMG} alt="Logo do site"/>
-                    <button 
-                    type="button"
-                    onClick={onOpenNewTransactionModal}>
-                        Nova transação
-                    </button>
-                </Content>
-            </Container>
-        </>
-    );
+export function Header() {
+  return (
+    <header className="h-header pt-8 bg-purple-500 px-6">
+      <div className="flex justify-between max-w-app mx-auto">
+        <div className="flex gap-4 items-center">
+          <div className=" flex h-10 w-10 bg-green-400 rounded-full items-center justify-center">
+            <FiDollarSign className="text-7 text-white" />
+          </div>
+          <h1 className="font-bold text-white text-2xl">dt money</h1>
+        </div>
+        <button className=" text-white rounded-md font-semibold py-3 px-8 drop-shadow-md shadow-md bg-purple-400  transition-all duration-[400ms] hover:cursor-pointer hover:bg-purple-300 hover:drop-shadow-2xl hover:shadow-lg"
+        >Nova transação</button>
+      </div>
+    </header>
+  )
 }
