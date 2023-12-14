5de456842ca15443d75c4e047633b26ff529f835
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-14 14:45:01 -0300

    :art: Fix import statements in Dashboard component :art:

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,20 +1,19 @@
-import { HighLightCard } from "../HighLightCard";
+import { HighLightCard } from '../HighLightCard'
 
-import * as Table from '../Table';
+import * as Table from '../Table'
 
 export function Dashboard() {
   return (
     <main className="h-dashboard bg-background-primary px-6">
-      <div className="max-w-app mx-auto">
-        <section className="flex gap-8 -mt-16">
+      <div className="mx-auto max-w-app">
+        <section className="-mt-16 flex gap-8">
           <HighLightCard value="20.000,00" />
           <HighLightCard variant="outcome" value="12.000,00" />
           <HighLightCard variant="total" value="32.000,00" />
         </section>
 
-
         <section className="mt-16">
-          <Table.Root >
+          <Table.Root>
             <Table.Header>
               <Table.Row variant="head">
                 <Table.Head>Título</Table.Head>
@@ -25,39 +24,51 @@ export function Dashboard() {
             </Table.Header>
             <Table.Body>
               <Table.Row variant="body">
-                <Table.Data variant="description">Desenvolvimento de website</Table.Data>
+                <Table.Data variant="description">
+                  Desenvolvimento de website
+                </Table.Data>
                 <Table.Data variant="income">R$ 12.000,00</Table.Data>
                 <Table.Data>Desenvolvimento</Table.Data>
                 <Table.Data>20/02/2021</Table.Data>
               </Table.Row>
 
               <Table.Row variant="body">
-                <Table.Data variant="description">Desenvolvimento de website</Table.Data>
+                <Table.Data variant="description">
+                  Desenvolvimento de website
+                </Table.Data>
                 <Table.Data variant="outcome">R$ 12.000,00</Table.Data>
                 <Table.Data>Desenvolvimento</Table.Data>
                 <Table.Data>20/02/2021</Table.Data>
               </Table.Row>
 
               <Table.Row variant="body">
-                <Table.Data variant="description">Desenvolvimento de website</Table.Data>
+                <Table.Data variant="description">
+                  Desenvolvimento de website
+                </Table.Data>
                 <Table.Data variant="income">R$ 12.000,00</Table.Data>
                 <Table.Data>Desenvolvimento</Table.Data>
                 <Table.Data>20/02/2021</Table.Data>
               </Table.Row>
               <Table.Row variant="body">
-                <Table.Data variant="description">Desenvolvimento de website</Table.Data>
+                <Table.Data variant="description">
+                  Desenvolvimento de website
+                </Table.Data>
                 <Table.Data variant="income">R$ 12.000,00</Table.Data>
                 <Table.Data>Desenvolvimento</Table.Data>
                 <Table.Data>20/02/2021</Table.Data>
               </Table.Row>
               <Table.Row variant="body">
-                <Table.Data variant="description">Desenvolvimento de website</Table.Data>
+                <Table.Data variant="description">
+                  Desenvolvimento de website
+                </Table.Data>
                 <Table.Data variant="outcome">R$ 12.000,00</Table.Data>
                 <Table.Data>Desenvolvimento</Table.Data>
                 <Table.Data>20/02/2021</Table.Data>
               </Table.Row>
               <Table.Row variant="body">
-                <Table.Data variant="description">Desenvolvimento de website</Table.Data>
+                <Table.Data variant="description">
+                  Desenvolvimento de website
+                </Table.Data>
                 <Table.Data variant="income">R$ 12.000,00</Table.Data>
                 <Table.Data>Desenvolvimento</Table.Data>
                 <Table.Data>20/02/2021</Table.Data>
