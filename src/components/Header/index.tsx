32e0bba62d5e128dc02a463d91086a9a9152370a
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-14 14:44:04 -0300

    :recycle: Add transaction form to header with new input and Select Components :recycle:

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,18 +1,78 @@
-import { FiDollarSign } from "react-icons/fi"
+import * as Dialog from '@radix-ui/react-dialog'
+import { FiX } from 'react-icons/fi'
 
+import * as Input from '../Input'
+
+import { Logo } from '../Logo'
+import { Button } from '../Button'
+import { Select } from '../Select'
+import { SelectItem } from '../Select/SelectItem'
+import { CATEGORIES } from '../../dto/categories'
 
 export function Header() {
+  const selectOptions = CATEGORIES.map((category) => (
+    <SelectItem key={category} text={category} value={category} />
+  ))
+
   return (
-    <header className="h-header pt-8 bg-purple-500 px-6">
-      <div className="flex justify-between max-w-app mx-auto">
-        <div className="flex gap-4 items-center">
-          <div className=" flex h-10 w-10 bg-green-400 rounded-full items-center justify-center">
-            <FiDollarSign className="text-7 text-white" />
-          </div>
-          <h1 className="font-bold text-white text-2xl">dt money</h1>
-        </div>
-        <button className=" text-white rounded-md font-semibold py-3 px-8 drop-shadow-md shadow-md bg-purple-400  transition-all duration-[400ms] hover:cursor-pointer hover:bg-purple-300 hover:drop-shadow-2xl hover:shadow-lg"
-        >Nova transação</button>
+    <header className="h-header bg-purple-400 px-6 pt-8">
+      <div className="mx-auto  flex max-w-app justify-between">
+        <Logo />
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <Button>Nova transação</Button>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay className="fixed inset-0 bg-gray-50">
+              <Dialog.Content
+                className={`border-0.5 fixed left-1/2 top-1/2 mx-auto my-0 flex 
+                 max-w-xl -translate-x-1/2 -translate-y-1/2 flex-col gap-4
+                 rounded-lg border-purple-500 bg-white px-12 py-16 `}
+              >
+                <Dialog.Close asChild className={`absolute right-2 top-2`}>
+                  <Button variant="ghost" className="group">
+                    <FiX
+                      className={`duration-400 transition-all 
+                      group-hover:text-purple-400`}
+                    />
+                  </Button>
+                </Dialog.Close>
+                <Dialog.Title className="text-2xl font-semibold text-gray-700">
+                  Cadastrar transação
+                </Dialog.Title>
+                <Dialog.Description className="text-lg text-gray-500">
+                  Preencha as informações abaixo para cadastrar uma nova
+                  transação
+                </Dialog.Description>
+                <form className="flex flex-col gap-4">
+                  <Input.Root>
+                    <Input.Control placeholder="Nome" />
+                  </Input.Root>
+                  <Input.Root>
+                    <Input.Control placeholder="Preço" />
+                  </Input.Root>
+
+                  <Select placeholder="Categoria">{selectOptions}</Select>
+
+                  <Input.Root>
+                    <Input.Control type="date" placeholder="Data" />
+                  </Input.Root>
+
+                  <Dialog.Close asChild className="mt-2">
+                    <Button
+                      type="submit"
+                      variant="secondary"
+                      className="w-full"
+                    >
+                      Cadastrar
+                    </Button>
+                  </Dialog.Close>
+                </form>
+              </Dialog.Content>
+            </Dialog.Overlay>
+          </Dialog.Portal>
+        </Dialog.Root>
       </div>
     </header>
   )
