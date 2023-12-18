718851a96eaac5a8e0f79acb234291f753e3929e
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2023-12-18 15:38:36 -0300

    ♻️💄 Refactor Select component styles with tailwind-variants 💄♻️

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,30 +1,59 @@
 import * as SelectPrimitive from '@radix-ui/react-select'
 import { ReactNode } from 'react'
 import { FiChevronDown } from 'react-icons/fi'
+import { VariantProps, tv } from 'tailwind-variants'
 
-type SelectProps = SelectPrimitive.SelectProps & {
-  children: ReactNode
-  placeholder: string
-}
+const select = tv({
+  slots: {
+    trigger: `group flex w-full cursor-pointer items-center justify-between 
+    rounded-lg border border-zinc-300 bg-background-secondary px-6 py-5 
+    shadow-sm outline-none transition-all duration-400
+    focus:border-purple-300 focus:ring-2 focus:ring-purple-300 hover:bg-white
+    data-[state=open]:bg-white data-[placeholder]:text-gray-500 
+    `,
+    icon: `text-xl text-gray-500 group-hover:text-purple-300`,
+  },
+  variants: {
+    variant: {
+      empty: {
+        trigger: '',
+        icon: '',
+      },
+      filled: {
+        trigger: 'bg-white',
+        icon: 'text-purple-300',
+      },
+    },
+  },
+  defaultVariants: {
+    variant: 'empty',
+  },
+})
 
-export function Select({ children, placeholder, ...rest }: SelectProps) {
+type SelectProps = SelectPrimitive.SelectProps &
+  VariantProps<typeof select> & {
+    children: ReactNode
+    placeholder: string
+  }
+
+export function Select({
+  children,
+  variant,
+  placeholder,
+  ...rest
+}: SelectProps) {
+  const { icon, trigger } = select({ variant })
+
   return (
     <SelectPrimitive.Root {...rest}>
-      <SelectPrimitive.Trigger
-        className={`bg-background-secondary group flex w-full cursor-pointer 
-        items-center justify-between rounded-lg border border-zinc-300 px-6 
-        py-5 shadow-sm outline-none focus:border-purple-300 
-        focus:ring-2 focus:ring-purple-300 data-[state=open]:bg-white 
-        data-[placeholder]:text-gray-500 
-        `}
-      >
+      <SelectPrimitive.Trigger id="select" className={trigger()}>
         <SelectPrimitive.Value
           placeholder={placeholder}
           className="text-gray-700"
         />
 
         <SelectPrimitive.Icon className="text-gray-500 group-data-[state=open]:text-purple-400">
-          <FiChevronDown className="text-xl group-hover:text-purple-400" />
+          <FiChevronDown className={icon()} />
         </SelectPrimitive.Icon>
       </SelectPrimitive.Trigger>
 
