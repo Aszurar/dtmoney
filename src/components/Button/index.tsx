169ed17f6f830189dc072d789f76005c6de16a2b
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-01-04 13:16:44 -0300

    :lipstick: Update Button component styles with dark mode :lipstick:

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,14 +8,16 @@ const button = tv({
   variants: {
     variant: {
       primary: `bg-white text-purple-400 shadow-md drop-shadow-md border-2 
-      border-transparent
+        border-transparent
         hover:bg-purple-400 hover:text-white hover:border-2 border-white
-        hover:shadow-lg hover:drop-shadow-xl`,
+        hover:shadow-lg hover:drop-shadow-xl dark:bg-zinc-950 
+        dark:text-purple-300 dark:hover:bg-purple-300 dark:border-purple-300
+        dark:hover:border-purple-200 dark:hover:text-white`,
       secondary: `py-5 bg-green-500 shadow-md drop-shadow-md 
         hover:bg-green-400 hover:shadow-lg hover:drop-shadow-xl`,
       ghost: `flex text-2xl px-0 py-0 text-gray-500 h-10 w-10 items-center 
       justify-center
-      hover:bg-zinc-200`,
+      hover:bg-zinc-200 dark:text-gray-400 dark:hover:bg-zinc-800`,
     },
   },
   defaultVariants: {
