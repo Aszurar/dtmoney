6ec0da9f3076dc3437feecb90a4a3f5a3762c1ba
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-01-04 13:20:03 -0300

    :lipstick: Refactor transaction type button styles to dark mode :lipstick:

--- src/components/Header/TransactionTypeButton/Root.tsx
+++ src/components/Header/TransactionTypeButton/Root.tsx
@@ -3,13 +3,17 @@ import { VariantProps, tv } from 'tailwind-variants'
 
 const root = tv({
   base: `group w-full py-5 px-4 border-0.375 border-background-tertiary flex gap-4 
-  items-center justify-center font-normal text-gray-700 rounded-md transition-all duration-400`,
+    items-center justify-center font-normal text-gray-700 rounded-md transition-all 
+    duration-400
+    dark:border-zinc-500 dark:text-white`,
   variants: {
     variant: {
       income: `hover:bg-green-75 hover:border-green-400`,
       outcome: `hover:bg-red-75 hover:border-red-500`,
-      incomeActive: `bg-green-75 border-green-400 hover:brightness-110`,
-      outcomeActive: `bg-red-75 border-red-500 hover:brightness-110`,
+      incomeActive: `bg-green-75 border-green-400 hover:brightness-110
+        dark:border-green-400`,
+      outcomeActive: `bg-red-75 border-red-500 hover:brightness-110
+        dark:border-red-500`,
     },
   },
 })
