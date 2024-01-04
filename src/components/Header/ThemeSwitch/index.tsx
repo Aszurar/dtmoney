7166e8d4eca553230e9cfb6a1c7f2d0f1c3627cf
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-01-04 13:25:14 -0300

    :sparkles: Add ThemeSwitch component :sparkles:

---
+++ src/components/Header/ThemeSwitch/index.tsx
@@ -0,0 +1,45 @@
+import { motion } from 'framer-motion'
+import { FiMoon, FiSun } from 'react-icons/fi'
+import { useTheme } from '../../../hook/useTheme'
+
+const SPRING_ANIMATION = {
+  type: 'spring',
+  stiffness: 700,
+  damping: 30,
+}
+
+export function ThemeSwitch() {
+  const { isDarkMode, handleToggleTheme } = useTheme()
+
+  const lightModeIcon = {
+    icon: FiSun,
+    label: 'Troca para tema Escuro',
+  }
+
+  const darkModeIcon = {
+    icon: FiMoon,
+    label: 'Trocar para tema Claro',
+  }
+
+  const { icon: Icon, label } = isDarkMode ? darkModeIcon : lightModeIcon
+
+  return (
+    <button aria-label={label} onClick={handleToggleTheme} className="self-end">
+      <div
+        data-theme={isDarkMode}
+        className={`flex h-10 w-20 items-center rounded-[90px] bg-gray-25 
+          p-2 shadow drop-shadow-sm focus:shadow-purple-200 
+          data-[theme=true]:justify-end`}
+      >
+        <motion.div
+          className={`flex h-8 w-8 cursor-pointer items-center justify-center 
+            rounded-full bg-white text-purple-300 dark:bg-zinc-900 `}
+          layout
+          transition={SPRING_ANIMATION}
+        >
+          <Icon className="h-6 w-6" />
+        </motion.div>
+      </div>
+    </button>
+  )
+}
