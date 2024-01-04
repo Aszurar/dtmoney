abcccb5d2df05be30e25f136718b52d4eabedca7
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-01-04 13:26:35 -0300

    :recycle: Refactor App.tsx and Dashboard/index.tsx to use transactions context and hook :recycle:

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import './theme/global.css'
 import { Header } from './components/Header'
 import { Dashboard } from './components/Dashboard'
 import { Footer } from './components/Footer'
+import { ThemeProvider } from './context/theme'
+import { TransactionsProvider } from './context/transactions'
 
 export function App() {
   return (
-    <div className=" flex min-h-screen flex-col antialiased">
-      <Header />
-      <Dashboard />
-      <Footer />
-    </div>
+    <ThemeProvider>
+      <TransactionsProvider>
+        <div className="flex min-h-screen flex-col antialiased">
+          <Header />
+          <Dashboard />
+          <Footer />
+        </div>
+      </TransactionsProvider>
+    </ThemeProvider>
   )
 }
--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,15 +1,42 @@
 import { HighLightCard } from '../HighLightCard'
 
 import * as Table from '../Table'
+import { priceFormatter, dateFormatter } from '../../utils/formatter'
+import { useTransactions } from '../../hook/useTransactions'
 
 export function Dashboard() {
+  const { transactions, balance, incomeTotal, outcomeTotal } = useTransactions()
+
+  function tableContentRows() {
+    return transactions.map((transaction) => {
+      const dataFormatted = dateFormatter.format(
+        new Date(transaction.createdAt),
+      )
+      const priceFormatted = priceFormatter.format(transaction.price)
+      const priceFormattedWithSignalOrNot =
+        transaction.type === 'outcome' ? `- ${priceFormatted}` : priceFormatted
+      return (
+        <Table.Row key={transaction.id} variant="body">
+          <Table.Data variant="description">
+            {transaction.description}
+          </Table.Data>
+          <Table.Data variant={transaction.type}>
+            {priceFormattedWithSignalOrNot}
+          </Table.Data>
+          <Table.Data>{transaction.category}</Table.Data>
+          <Table.Data>{dataFormatted}</Table.Data>
+        </Table.Row>
+      )
+    })
+  }
+
   return (
-    <main className="h-dashboard bg-background-primary px-6">
+    <main className="h-dashboard bg-background-primary px-6 dark:bg-zinc-950">
       <div className="mx-auto max-w-app">
         <section className="-mt-16 flex gap-8">
-          <HighLightCard value="20.000,00" />
-          <HighLightCard variant="outcome" value="12.000,00" />
-          <HighLightCard variant="total" value="32.000,00" />
+          <HighLightCard value={incomeTotal} />
+          <HighLightCard variant="outcome" value={outcomeTotal} />
+          <HighLightCard variant="total" value={balance} />
         </section>
 
         <section className="mt-16">
@@ -22,58 +49,7 @@ export function Dashboard() {
                 <Table.Head>Data</Table.Head>
               </Table.Row>
             </Table.Header>
-            <Table.Body>
-              <Table.Row variant="body">
-                <Table.Data variant="description">
-                  Desenvolvimento de website
-                </Table.Data>
-                <Table.Data variant="income">R$ 12.000,00</Table.Data>
-                <Table.Data>Desenvolvimento</Table.Data>
-                <Table.Data>20/02/2021</Table.Data>
-              </Table.Row>
-
-              <Table.Row variant="body">
-                <Table.Data variant="description">
-                  Desenvolvimento de website
-                </Table.Data>
-                <Table.Data variant="outcome">R$ 12.000,00</Table.Data>
-                <Table.Data>Desenvolvimento</Table.Data>
-                <Table.Data>20/02/2021</Table.Data>
-              </Table.Row>
-
-              <Table.Row variant="body">
-                <Table.Data variant="description">
-                  Desenvolvimento de website
-                </Table.Data>
-                <Table.Data variant="income">R$ 12.000,00</Table.Data>
-                <Table.Data>Desenvolvimento</Table.Data>
-                <Table.Data>20/02/2021</Table.Data>
-              </Table.Row>
-              <Table.Row variant="body">
-                <Table.Data variant="description">
-                  Desenvolvimento de website
-                </Table.Data>
-                <Table.Data variant="income">R$ 12.000,00</Table.Data>
-                <Table.Data>Desenvolvimento</Table.Data>
-                <Table.Data>20/02/2021</Table.Data>
-              </Table.Row>
-              <Table.Row variant="body">
-                <Table.Data variant="description">
-                  Desenvolvimento de website
-                </Table.Data>
-                <Table.Data variant="outcome">R$ 12.000,00</Table.Data>
-                <Table.Data>Desenvolvimento</Table.Data>
-                <Table.Data>20/02/2021</Table.Data>
-              </Table.Row>
-              <Table.Row variant="body">
-                <Table.Data variant="description">
-                  Desenvolvimento de website
-                </Table.Data>
-                <Table.Data variant="income">R$ 12.000,00</Table.Data>
-                <Table.Data>Desenvolvimento</Table.Data>
-                <Table.Data>20/02/2021</Table.Data>
-              </Table.Row>
-            </Table.Body>
+            <Table.Body>{tableContentRows()}</Table.Body>
           </Table.Root>
         </section>
       </div>
