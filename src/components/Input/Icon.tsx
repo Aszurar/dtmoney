35196dce75db5954135e29e8d5f4dada5754344c
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-01-16 15:32:05 -0300

    📱 Improve Responsiveness with Input 📱

--- src/components/Input/Icon.tsx
+++ src/components/Input/Icon.tsx
@@ -2,12 +2,12 @@ import { ComponentProps, ElementType } from 'react'
 import { VariantProps, tv } from 'tailwind-variants'
 
 const icon = tv({
-  base: `text-gray-500 group-focus-within:text-purple-300 
+  base: `text-xl text-gray-500 group-focus-within:text-purple-300 
     dark:text-gray-400 dark:group-focus-within:text-purple-200`,
   variants: {
     variant: {
       empty: '',
-      filled: `text-xl text-purple-300 dark:text-purple-200`,
+      filled: ` text-purple-300 dark:text-purple-200`,
     },
   },
   defaultVariants: {
--- src/components/Input/Root.tsx
+++ src/components/Input/Root.tsx
@@ -2,8 +2,8 @@ import { ComponentProps } from 'react'
 import { VariantProps, tv } from 'tailwind-variants'
 
 const root = tv({
-  base: `group flex w-full items-center gap-2 rounded-md border 
-  border-background-tertiary bg-background-secondary px-6 py-5 
+  base: `group flex w-full items-center gap-2 rounded-md border px-6 py-5
+  border-background-tertiary bg-background-secondary  
   focus-within:border-purple-300 focus-within:ring-2 focus-within:ring-purple-300
   dark:bg-zinc-800 dark:border-zinc-500 dark:focus-within:border-purple-200
   dark:focus-within:ring-purple-200
