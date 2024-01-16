d4908b332bc7faadc57efcd55b5b56e8a64a5196
Lucas de Lima Martins de Souza <lms.souza39@gmail.com>
2024-01-16 15:28:40 -0300

    📱 Improve responsiveness 📱

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,20 +2,21 @@ import { ComponentProps } from 'react'
 import { VariantProps, tv } from 'tailwind-variants'
 
 const button = tv({
-  base: `rounded-md px-5 py-3 font-semibold text-white  transition-all 
-  duration-400 bg-transparent cursor-pointer
+  base: `rounded-md px-5 py-2 font-semibold text-white transition-all 
+  duration-400 bg-transparent cursor-pointer disabled:opacity-50 
+  disabled:cursor-not-allowed
    `,
   variants: {
     variant: {
-      primary: `bg-white text-purple-400 shadow-md drop-shadow-md border-2 
+      primary: ` bg-white text-purple-400 shadow-md drop-shadow-md border-2 
         border-transparent text-sm phone-xl:text-base phone-xl:px-8 border-white
         hover:bg-purple-400 hover:text-white hover:border-2 hover:shadow-lg 
-        hover:drop-shadow-xl 
-        dark:bg-zinc-950 dark:text-purple-300 dark:hover:bg-purple-300 
-        dark:border-purple-300 dark:hover:border-purple-200 
-        dark:hover:text-white`,
-      secondary: `py-5 bg-green-500 shadow-md drop-shadow-md 
-        hover:bg-green-400 hover:shadow-lg hover:drop-shadow-xl`,
+        hover:drop-shadow-xl phone-xl:py-3 
+        dark:bg-zinc-950 dark:text-purple-300 dark:border-transparent 
+        dark:hover:border-white dark:hover:text-white dark:hover:bg-purple-400`,
+      secondary: `flex py-3 sm:py-5 bg-green-500 shadow-md drop-shadow-md 
+        hover:bg-green-400 hover:shadow-lg hover:drop-shadow-xl justify-center 
+        items-center`,
       ghost: `flex text-2xl px-0 py-0 text-gray-500 h-10 w-10 items-center 
         justify-center
         hover:bg-zinc-200 hover:text-gray-700
